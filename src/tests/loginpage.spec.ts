60d4c20d0f224aa1e26fa2d89b676a935c0f413d
Vijay Durairaj <vijaydrrj@gmail.com>
2026-03-02 22:42:17 -0600

    Update to spec file to read the test data
    - remove DataHelper class and TestDataTypes.ts
    - reading the test data directly from the .json file

--- src/tests/loginpage.spec.ts
+++ src/tests/loginpage.spec.ts
@@ -2,26 +2,24 @@ import { LoginPage } from '@pages/LoginPage';
 import { test, expect } from '@playwright/test';
 import { container, LoginPageFactory } from '@symbols/loginpage/LoginPage.inversify';
 import { LOGIN_PAGE } from '@symbols/loginpage/LoginPage.symbol';
-import { DataHelper } from 'src/helper/DataHelper'; 
-import {
-    TestData, LoginTestData,
-} from '../utils/TestDataTypes';
+import loginTs from '../resource/testdata/LoginPage.json';
+import testData from '../resource/testdata/LoginPage.json';
 
 test.describe('Login Page', () => {
 
-    let loginPage: LoginPage;  
-    const urls      = DataHelper.getField<TestData, TestData['urls']>('testdata.json', 'urls');
-    const loginData = DataHelper.getAll<TestData, LoginTestData>('testdata.json', 'loginData');
+    const homeUrl = testData.urls[0];
+    const loginData = loginTs.loginData;
+    let loginPage: LoginPage;
 
     test.beforeEach(async ({ page }) => {
-         loginPage = container.get<LoginPageFactory>(LOGIN_PAGE.LoginPageFactory)(page);
+        loginPage = container.get<LoginPageFactory>(LOGIN_PAGE.LoginPageFactory)(page);
     });
 
-    loginData.forEach((loginData) => {
-        test((`${loginData.testId}, ${loginData.description}`), async ({ page }) => {
-            await page.goto(urls.login);
+    loginData.forEach((testData) => {
+        test((`${testData.testId}, ${testData.description}`), async ({ page }) => {
+            await page.goto(homeUrl.login);
             await expect(page.locator('form')).toBeVisible();
-            await loginPage.login(loginData.email, loginData.password); 
+            await loginPage.login(testData.email, testData.password);
             await page.waitForLoadState('networkidle');
             expect(await loginPage.isLoginSuccessful()).toBeTruthy();
             await loginPage.logout();
